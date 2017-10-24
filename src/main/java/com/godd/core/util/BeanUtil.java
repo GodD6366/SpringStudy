64126c25e7fa0435e82917bddd583fc3a67f9cab
daichangchun6366@gmail.com <daichangchun6366@gmail.com>
2017-10-24 14:29:38 +0800

    setter工具类

---
+++ src/main/java/com/godd/core/util/BeanUtil.java
@@ -0,0 +1,13 @@
+package com.godd.core.util;
+
+/**
+ * Description:
+ * User: godd
+ * Date: 2017-10-24
+ * Time: 上午11:07
+ */
+public class BeanUtil {
+    public static String parseSetter(String name){
+        return "set"+name.substring(0,1).toUpperCase()+name.substring(1);
+    }
+}
